a91f9f8cf05360ea634d994a90f9cc9334619f81
Ängla Juliusson <angla02@icloud.com>
2026-01-29 11:53:04 +0100

    Registrera routes i server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors' // Importera cors
 import './config/db.js'
+import { routes as todosRoutes } from './routes/todosRoutes.js'; // Importera route-funktionerna från todosRoutes.js
 
 const fastify = Fastify({ logger: true }); 
 
@@ -9,6 +10,9 @@ const fastify = Fastify({ logger: true });
 await fastify.register(cors, {
   origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] // Tillåt alla origins (utveckling) och alla metoder
 })
+
+// Registrera todosRoutes
+fastify.register(todosRoutes);
 
 // Run the server!
 try {
